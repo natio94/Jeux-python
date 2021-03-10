9f32a64befc5a34e72b60387adf802a992a52408
natio94 <natemps.io@gmail.com>
2021-03-10 08:17:25 +0100

    Amélioration du code

--- PENDU.py
+++ PENDU.py
@@ -4,46 +4,48 @@ mot = ""
 motsConnu = []
 lettresDonnees = []
 
+
 def afficheNouvelleLettre(lettreAAficher):
-    i=0
+    i = 0
     for lettre in mot:
-        if(lettre==lettreAAficher):
-            motsConnu[i]=lettreAAficher
-        i+=1
+        if(lettre == lettreAAficher):
+            motsConnu[i] = lettreAAficher
+        i += 1
+
 
 while True:
     while True:
         print("Choisissez un mot que votre adversaire doit deviner . ")
-        mot=input("")
+        mot = input("")
         print("Le mot que vous avez choisi est "+mot+". Voulez vous le changer ? (n ou 2 pour non toutes les autres touches pour oui")
-        rep=input("")
-        if rep=="2"or rep=="n":
+        rep = input("")
+        if rep == "2" or rep == "n":
             break
     # disp_clr()
-    nbLettre=len(mot)
+    nbLettre = len(mot)
     motsConnu = ['_']*nbLettre
 
-    while erreur<11:
+    while erreur < 11:
         print("Choisissez une lettre.")
-        lettre=input("")
+        lettre = input("")
         if lettre in lettresDonnees:
             print("Vous avez deja donné cette lettre.")
         else:
             afficheNouvelleLettre(lettre)
             if lettre in mot:
                 print("Cette lettre est dans le mot")
-                s=""
+                s = ""
                 print("Le mot actuel est : "+s.join(motsConnu))
             else:
-                erreur+=1
+                erreur += 1
                 print("La lettre n'est pas dans le mot. Il vous reste "+str(11-erreur)+" chance.")
         lettresDonnees.append(lettre)
-        if erreur==11:
+        if erreur == 11:
             print("Vous avez perdu.")
         if '_' not in motsConnu:
             print("Bravo !!! Vous avez gagné")
             break
     print("Voulez-vous recommencer ? (n ou 2 pour non toutes les autres touches pour oui)")
-    rep=input("")
-    if rep=="2"or rep=="n":
+    rep = input("")
+    if rep == "2" or rep == "n":
         break
